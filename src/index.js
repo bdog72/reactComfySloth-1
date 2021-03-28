eb6f7c51130803bb4c98076360ccca7c2a3bbc84
Brian Bycynski <high5bri@hotmail.com>
2021-03-28 15:53:34 -0400

    ready to deploy

--- src/index.js
+++ src/index.js
@@ -30,8 +30,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <Auth0Provider
-    domain='dev-u05q57yo.us.auth0.com'
-    clientId='uwpbwWXFzNmX7Wa2t8g1FsqFmQb4taOk'
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
     redirectUri={window.location.origin}
     cacheLocation='localstorage'
   >
